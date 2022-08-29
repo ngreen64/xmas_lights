c0628ae24c388cb66f9ad6e07985eed709882c65
ngreen64 <me@example.com>
2022-08-29 14:41:08 +0100

    added a executable to turn lights off

---
+++ xlights_off.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+import board
+import neopixel
+
+# variables
+no_of_lights=100
+
+pixels = neopixel.NeoPixel(board.D18, no_of_lights, auto_write=False)
+for i in range(no_of_lights):
+   pixels[i]= [0,0,0]
+
+pixels.show()
